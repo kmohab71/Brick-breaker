542b5013ef2b7327023f7a0736d64662a58a4e02
khaledmohab1 <kmohab71@yahoo.com>
2020-03-19 00:53:26 +0200

    Initial Commit

---
+++ project1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  project1
+//
+//  Created by Khaled Mohab on 3/19/20.
+//  Copyright © 2020 Khaled Mohab. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
